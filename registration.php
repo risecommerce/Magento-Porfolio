90d2a49049ffcc146661d25ad9853aac578a0a4f
Amar142 <prajapatiamar142@gmail.com>
2023-06-22 17:55:26 +0530

    Module Portfolio

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * * @copyright Copyright (c) 2017-2021 Risecommerce Pvt Ltd. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Risecommerce_Portfolio',
+    __DIR__
+);
